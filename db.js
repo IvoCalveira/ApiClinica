f1882ca52df5befa0132542be77c9d808ad82018
IvoCalveira <calveira.ivo@gmail.com>
2024-06-07 14:42:51 -0300

    first commit

--- db.js
+++ db.js
@@ -41,25 +41,29 @@ exports.insertarPersona = function(usuario, retornar){
            
            const idmedico = resultado.insertId;
            if(usuario.tipo_usuario == 2){
+            //acá inserta en la tabla médicos
                var sql2 = "INSERT INTO medico (id_usuario, especialidad, foto_especialidad, autorizado) VALUES (?, ?, ?, ?)";
                var medico = [idmedico, usuario.especialidad, usuario.foto_especialidad, usuario.autorizado];
                conexion.query(sql2, medico,
                 function(err, resultadomedico, filas){
                     if(err) throw err;
                     console.log(resultadomedico);
-                    
-                    return retornar(resultadomedico);
-                });
+            //acá inserta en la tabla dias_habiles  
                 const idhabiles = resultadomedico.insertId;
-                insertardias(usuario, idhabiles);
+                var sql3 = "INSERT INTO dias_habiles (id_medico, lunes, martes, miercoles, jueves, viernes, horas_desde, horas_hasta) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+                var dias = [idhabiles, usuario.dias_habiles[0], usuario.dias_habiles[1], usuario.dias_habiles[2], usuario.dias_habiles[3], usuario.dias_habiles[4], usuario.horario_desde, usuario.horario_hasta];
+                conexion.query(sql3, dias,
+                    function(err, resultadodias, filas){
+                        if(err) throw err;
+                        console.log(resultadodias);
+                        
+                        return retornar(resultadodias);
+                    });
+                });
             }
         });
 }
 
-insertardias = function(usuario, retornar){
-    var sql = "INSERT INTO dias_habiles (id_medico, lunes, martes, miercoles, jueves, viernes, horario_desde, horas_hasta) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
-    var values = [idhabiles, usuario.horario_desde, usuario.horario_hasta];
-}
 
 exports.borrarPersona = function(usuario, retornar){
     conectar();
